58576ff52ef6faa7204eaf77220cada6e1080e89
Hartej Nayar <71260380+hartejnayar@users.noreply.github.com>
2022-12-29 15:45:58 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent("car", {
+    schema:{
+        modelRef: {type:"string", default: "../assets/car/scene.gltf"},
+        clickCounter: {type: "number", default: 0}
+    },
+
+    init: function(){
+        this.el.setAttribute("gltf-model", this.data.modelRef);
+
+        const position = {x: 0, y: 50, z: 80};
+        const rotation = {x: 0, y: -100, z: 0};
+
+        this.el.setAttribute("position", position);
+        this.el.setAttribute("rotation", rotation);
+    },
+
+    update: function(){
+        window.addEventListener("click", (e) => {
+            this.data.clickCounter += 1;
+            if(this.data.clickCounter === 1) {
+                const rotation = {x: 0, y: 20, z: 0};
+                this.el.setAttribute("rotation", rotation)
+            }
+           
+            else if(this.data.clickCounter === 2) {
+                const rotation = { x: 0, y: 100, z: 0 };
+                this.el.setAttribute("rotation", rotation);
+            }
+
+            else if(this.data.clickCounter === 3) {
+                const rotation = { x: 0, y: -200, z: 0 };
+                this.el.setAttribute("rotation", rotation); 
+            }
+
+            else if(this.data.clickCounter === 4) {
+                const position = { x: 0, y: 195, z: 450 };
+                this.el.setAttribute("rotation", rotation);
+            }
+
+            else { 
+                const rotation = {x: 0 , y: -100, z: 0};
+                this.el.setAttribute("rotation", rotation)
+                this.data.clickCounter = 0;
+            }
+            
+        })
+    }
+})  
